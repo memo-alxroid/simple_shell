a559a51b94eca4706ff8e4ac783196ec41cbdd9f
Mina Sami <memo.samy0050@gmail.com>
2023-11-25 16:32:39 -0800

    handling fork

--- main.c
+++ main.c
@@ -1,13 +1,27 @@
 #include "main.h"
 
 
-int main()
+int main(int argc, char *argv[])
 {
 	char **commandsArray;
+
+	(void)argc;
+
+	if(fork() == 0 )
+	{
+		printf("inside child");
+		execve("usr/bin/clear", argv, NULL);
+	}
+	else
+	{
+		printf("inside parent");
+		wait(NULL);
+	}
 
 	while (1)
 	{
 		printf("#cisfun$ ");
+		fflush(stdout);
 		commandsArray = extract_commands_array();
 		if (commandsArray == NULL)
 		{
--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <math.h>
 
